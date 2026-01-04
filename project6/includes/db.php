9bf198fb1f767b2d36a8f6f5f5eb11263b80d2ce
Afshin <afshin77amn@gmail.com>
2026-01-05 01:23:57 +0330

    Delete project6 directory

--- project6/includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "project";
-$conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
-?>
--- project6/pages/auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_abort();
-$errMsg='لطفا ابتدا وارد شوید';
-    header("Location:login.php?errMsg=$errMsg");
-?>
